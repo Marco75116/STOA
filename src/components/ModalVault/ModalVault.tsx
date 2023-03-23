5c614be04c83a44fd37397df60a6fc2e46ec3d48
marc-antoine ourradour <marc-antoineourradour@MacBook-Air-de-marc-antoine.local>
2023-03-23 08:40:39 +0100

    modal on vault page

---
+++ src/components/ModalVault/ModalVault.tsx
@@ -0,0 +1,153 @@
+import { Dialog, Transition } from "@headlessui/react";
+import { FC, Fragment } from "react";
+import { ReactComponent as Cross } from "../../assets/icons/Cross.svg";
+import { ReactComponent as DoubleArrow } from "../../assets/icons/DoubleArrow.svg";
+import { ReactComponent as Info } from "../../assets/icons/Info.svg";
+import { ReactComponent as RoundCross } from "../../assets/icons/RoundCross.svg";
+
+type ModalVaultProps = {
+  isOpen: boolean;
+  setIsOpen: Function;
+  setIsVaultOpen: Function;
+};
+
+const ModalVault: FC<ModalVaultProps> = ({
+  isOpen,
+  setIsOpen,
+  setIsVaultOpen,
+}) => {
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  return (
+    <>
+      <div className="">
+        <div className=" flex h-[200px] w-[459px] flex-row justify-between  rounded-2xl border-[0.5px] border-solid border-borderCardAbout bg-white p-5">
+          <div className="flex w-[235px] flex-col self-center ">
+            <div className="text-[20px] font-semibold">
+              There are no vaults yet
+            </div>
+            <div className="text-[14px] text-[#000000B2]  ">
+              Create your first vault to explore all the features of STOA
+            </div>
+          </div>
+          <div
+            className=" flex h-[160px] w-[160px] flex-col items-center justify-center rounded-2xl bg-[#EF2A891F] text-pink hover:cursor-pointer"
+            onClick={openModal}
+          >
+            New Vault
+          </div>
+        </div>
+      </div>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="bg-black fixed inset-0 bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="h-[536px] w-full max-w-md transform overflow-hidden rounded-2xl border-[0.5px] border-solid border-borderCardAbout bg-white text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-gray-900 flex flex-row items-center justify-between border-b-[0.5px] border-solid border-borderCardAbout  p-6 text-lg font-medium leading-6"
+                  >
+                    New Vault
+                    <Cross />
+                  </Dialog.Title>
+                  <div className="flex h-[372px] flex-col justify-between border-b-[0.5px] border-solid border-borderCardAbout px-[20px] py-[32px]">
+                    <div className="flex h-[68px] flex-col justify-between">
+                      <div>Vault Name</div>
+                      <div className="h-[40px] rounded-xl border-[0.5px] border-solid border-borderCardAbout p-[10px] text-textGray">
+                        USDFI Vault
+                      </div>
+                    </div>
+                    <div className="grid grid-cols-1 rounded-xl border-[0.5px] border-solid border-borderCardAbout ">
+                      <div className=" flex justify-between p-[20px] pb-[4px] ">
+                        <div>
+                          <div>1000</div>
+                          <div className="text-textGray">$1,000.00</div>
+                        </div>
+                        <div>
+                          <div>{"USDC " + ">"}</div>
+                          <div className="text-textGray">balance: 1,000</div>
+                        </div>
+                      </div>
+                      <div className="flex justify-center">
+                        <DoubleArrow />
+                      </div>
+                      <div className=" flex justify-between p-[20px] pt-[4px] ">
+                        <div>
+                          <div>996</div>
+                          <div className="text-textGray">$995.80</div>
+                        </div>
+                        <div>
+                          <div>USDFI</div>
+                          <div className="text-textGray">balance: 1,000</div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-row justify-between rounded-xl bg-ethBalance p-2">
+                      <div className="flex flex-row items-center">
+                        <Info />
+                        <div>
+                          1 USDC = 1.0006 USDFI{" "}
+                          <span className="text-textGray">{"($1.000)"}</span>
+                        </div>
+                      </div>
+                      <div className="flex flex-row items-center">
+                        <RoundCross />
+                        <div className="text-textGray">$6.20</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="p-[20px]">
+                    <div
+                      className="flex h-[48px] items-center justify-center rounded-lg bg-pink p-3 text-base font-normal text-white hover:cursor-pointer
+                      "
+                    >
+                      <button
+                        type="button"
+                        onClick={() => {
+                          closeModal;
+                          setIsVaultOpen(true);
+                        }}
+                      >
+                        Open Vaults
+                      </button>
+                    </div>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+};
+
+export default ModalVault;
--- src/pages/VaultsPage/VaultsPage.tsx
+++ src/pages/VaultsPage/VaultsPage.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import ModalVault from "../../components/ModalVault/ModalVault";
+import { ReactComponent as RedBulb } from "../../assets/icons/RedBulb.svg";
 
 const VaultsPage = () => {
-  return <div>VaultsPage</div>;
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [isVaultOpen, setIsVaultOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      {!isVaultOpen ? (
+        <div className="flex h-[calc(100%-64px)] flex-col items-center justify-center gap-[15px]">
+          <ModalVault
+            isOpen={isOpen}
+            setIsOpen={setIsOpen}
+            setIsVaultOpen={setIsVaultOpen}
+          />
+          <div className=" flex h-[156px] w-[459px] flex-col justify-between  rounded-2xl border-[0.5px] border-solid border-borderCardAbout bg-white p-5">
+            <RedBulb />
+            <div className="gap-[8px]">
+              <div className=" text-base font-semibold ">
+                Deposit and Borrow
+              </div>
+              <div className="text-[14px] font-normal text-[#000000B2]">
+                Stoa Vaults enable you to borrow credit against your
+                yield-bearing stablecoins, and earn more points. See here for
+                more details.
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>vault </div>
+      )}
+    </>
+  );
 };
 
 export default VaultsPage;
