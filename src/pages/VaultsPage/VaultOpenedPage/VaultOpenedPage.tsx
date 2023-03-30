2fcd58666d379b52851dbf0cb9fee0134bc1c373
marc-antoine ourradour <marc-antoineourradour@MacBook-Air-de-marc-antoine.local>
2023-03-30 20:08:06 +0200

    vault Page style

--- src/pages/VaultsPage/VaultOpenedPage/VaultOpenedPage.tsx
+++ src/pages/VaultsPage/VaultOpenedPage/VaultOpenedPage.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
-import { ReactComponent as ArrowWhite } from "../../../assets/icons/ArrowWhite.svg";
 import { ReactComponent as Arrow } from "../../../assets/icons/Arrow.svg";
 import { ReactComponent as Bulb } from "../../../assets/icons/Bulb.svg";
+import { ReactComponent as Cadena } from "../../../assets/icons/Cadena.svg";
 import { ReactComponent as LegendGraph } from "../../../assets/texts/StoaEarning.svg";
 import { ReactComponent as SamsVault } from "../../../assets/logos/SamsVault.svg";
+import { ReactComponent as Info } from "../../../assets/icons/Info.svg";
+import { ReactComponent as StoaLogoBlack } from "../../../assets/logos/STOALOGOBLACK.svg";
 import Graph from "../../../components/Graph/Graph";
+import Example from "../../../components/ListBoxActions/ListBoxActions";
 
 const VaultOpenedPage = () => {
   const [period, setPeriod] = useState<"days" | "months" | "years">("days");
@@ -19,14 +22,8 @@ const VaultOpenedPage = () => {
             </div>
             <Arrow />
           </div>
-          <div
-            onClick={() => {}}
-            className="flex h-[32px] w-[99px]  flex-row items-center justify-between rounded-lg bg-pink p-2  text-xs font-normal text-white hover:cursor-pointer
-                      "
-          >
-            Take Action
-            <ArrowWhite />
-          </div>
+
+          <Example />
         </div>
 
         <div className="grid grid-cols-4 ">
@@ -34,7 +31,8 @@ const VaultOpenedPage = () => {
             <div className="flex flex-col justify-between">
               <div className="text-textGray">balance</div>
               <div>
-                10,000.00 <span className=" text-textGray">USDFI</span>
+                <span className="mr-[4px] text-2xl font-bold"> 10,000.00</span>
+                <span className=" text-textGray">USDFI</span>
               </div>
             </div>
           </div>
@@ -43,54 +41,66 @@ const VaultOpenedPage = () => {
             <div className="flex flex-col justify-between">
               <div className="text-textGray">APY</div>
               <div>
-                3.5% <span className=" text-textGray">30d AER V </span>
+                <span className="mr-[4px] text-2xl font-bold"> 3.5%</span>
+                <span className=" text-textGray">30d AER V </span>
               </div>
             </div>
           </div>
 
           <div className="border-b-[0.5px] border-t-[0.5px]  border-r-[0.5px] border-solid border-borderCardAbout p-[20px]">
             <div className="flex flex-col justify-between">
-              <div className="text-textGray">Yield Earcned</div>
+              <div className="text-textGray">Yield Earned</div>
               <div>
-                178 <span className=" text-textGray">USDFI</span>
+                <span className="mr-[4px] text-2xl font-bold"> 178</span>
+                <span className=" text-textGray">USDFI</span>
               </div>
             </div>
           </div>
 
           <div className="border-b-[0.5px] border-t-[0.5px]  border-solid border-borderCardAbout p-[20px]">
             <div className="flex flex-col justify-between">
-              <div className="text-textGray">Points Earned</div>
+              <div className="flex items-center gap-2 text-textGray">
+                <StoaLogoBlack /> Points Earned
+              </div>
               <div>
-                54 <span className=" text-textGray">STOA</span>
+                <span className="mr-[4px] text-2xl font-bold"> 54</span>
+                <span className=" text-textGray">STOA</span>
               </div>
             </div>
           </div>
         </div>
 
         <div className="flex flex-row">
-          <div className="flex h-[96px] w-[208px] flex-col justify-between p-5 ">
-            <div className="flex flex-row items-center">
-              <SamsVault /> Stoa Credit
+          <div className="flex h-[96px] w-[218px] flex-col justify-between p-5 ">
+            <div className="flex items-center gap-2">
+              <SamsVault /> Stoa Credit <Info />
             </div>
             <div>
-              2500 <span className=" text-textGray">/2500 USDC</span>
+              <span className="mr-[4px] text-2xl font-bold">2500</span>
+              <span className=" text-sm font-normal text-textGray">
+                /2500 USDC
+              </span>
             </div>
           </div>
+
           <div className="flex w-[100%] flex-row justify-between p-5 pl-[72px]">
             <div className="flex flex-col justify-between">
-              <div>Locked Amount </div>
+              <div className="flex items-center gap-2 ">
+                <Cadena /> Locked Amount <Info />
+              </div>
               <div>
-                0,00 <span className=" text-textGray">USDFI</span>
+                <span className="mr-[4px] text-2xl font-bold"> 0,00</span>
+                <span className="text-sm font-normal text-textGray">USDFI</span>
               </div>
             </div>
             <div className="flex h-[28px] w-[173px] gap-[4px]">
-              <div className=" flex items-center rounded-lg border-[0.5px] border-solid border-borderCardAbout px-[8px] py-[4px] text-xs ">
+              <div className=" flex items-center rounded-lg border-[0.5px] border-solid border-borderCardAbout px-[8px] py-[4px] text-xs font-semibold hover:cursor-pointer ">
                 Borrow
               </div>
-              <div className=" flex items-center rounded-lg border-[0.5px] border-solid border-borderCardAbout px-[8px] py-[4px] text-xs">
+              <div className=" flex items-center rounded-lg border-[0.5px] border-solid border-borderCardAbout px-[8px] py-[4px] text-xs font-semibold hover:cursor-pointer">
                 Repay
               </div>
-              <div className="flex items-center rounded-lg border-[0.5px] border-solid border-borderCardAbout px-[8px] py-[4px] text-xs">
+              <div className="flex items-center rounded-lg border-[0.5px] border-solid border-borderCardAbout bg-[#EF2A891F] px-[8px] py-[4px] text-xs font-semibold text-pink hover:cursor-pointer">
                 Transfer
               </div>
             </div>
