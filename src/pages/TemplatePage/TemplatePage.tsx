faad9f4c6fa84f48658ee55d02bf9b44c57b521e
marc-antoine ourradour <marc-antoineourradour@airdemacantoine.home>
2023-03-31 10:48:41 +0200

    test the deployement of netlify

--- src/pages/TemplatePage/TemplatePage.tsx
+++ src/pages/TemplatePage/TemplatePage.tsx
@@ -9,7 +9,9 @@ const TemplatePage = () => {
     <>
       <div className="flex h-screen w-screen flex-col">
         <div className="flex flex-col items-center">
-          <h1 className="p-10 text-center text-3xl">Tailwind Template </h1>
+          <h1 className="p-10 text-center text-3xl">
+            Tailwind Template test netlify{" "}
+          </h1>
           <div
             onClick={() => setIsOpen(true)}
             className="w-50 bg-pink-200 hover:bg-pink-300 animate-bounce cursor-pointer rounded-md p-4 shadow-lg"
